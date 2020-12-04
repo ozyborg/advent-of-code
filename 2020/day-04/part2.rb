e2090d2059212c93fcbbe2b739fe776afaece80d
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-04 17:05:15 +0100

    Day 04

--- 2020/day-04/part2.rb
+++ 2020/day-04/part2.rb
@@ -5,13 +5,13 @@ def process(data)
     passport = d.split(/[\n\s]+/).map { |p| p.split(':') }.to_h
 
     (required_fields - passport.keys).empty? &&
-    (passport['byr'].match(/^(19[2-9][0-9])|(200[0-2])$/)) &&
-    (passport['iyr'].match(/^(201[0-9])|(2020)$/)) &&
-    (passport['eyr'].match(/^(202[0-9])|(2030)$/)) &&
-    (passport['hgt'].match(/^(1(([5-8][0-9])|(9[0-3]))cm)|(((59)|(6[0-9])|(7[0-6]))in)$/)) &&
-    (passport['hcl'].match(/^#[0-9a-f]{6}$/)) &&
-    (passport['ecl'].match(/^amb|blu|brn|gry|grn|hzl|oth$/)) &&
-    (passport['pid'].match(/^[0-9]{9}$/))
+    passport['byr'].match(/^(19[2-9][0-9])|(200[0-2])$/) &&
+    passport['iyr'].match(/^(201[0-9])|(2020)$/) &&
+    passport['eyr'].match(/^(202[0-9])|(2030)$/) &&
+    passport['hgt'].match(/^(1(([5-8][0-9])|(9[0-3]))cm)|(((59)|(6[0-9])|(7[0-6]))in)$/) &&
+    passport['hcl'].match(/^#[0-9a-f]{6}$/) &&
+    passport['ecl'].match(/^amb|blu|brn|gry|grn|hzl|oth$/) &&
+    passport['pid'].match(/^[0-9]{9}$/)
   end
 end
 
