de33f7ba7d6245a140f46474940b0ea298943092
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-04 16:30:21 +0100

    Day 04

--- 2020/day-04/part2.rb
+++ 2020/day-04/part2.rb
@@ -2,38 +2,18 @@ def process(data)
   required_fields = %w(byr iyr eyr hgt hcl ecl pid)
 
   data.count do |d|
-    passport_fields = d.split(/[\n\s]+/).map { |p| p.split(':') }.to_h
+    passport = d.split(/[\n\s]+/).map { |p| p.split(':') }.to_h
 
-    next false unless (required_fields - passport_fields.keys).empty?
-
-    passport_fields.all? do |field, value|
-      case field
-      when 'byr'
-        value.to_i >= 1920 && value.to_i <= 2002
-      when 'iyr'
-        value.to_i >= 2010 && value.to_i <= 2020
-      when 'eyr'
-        value.to_i >= 2020 && value.to_i <= 2030
-      when 'hgt'
-        if value.end_with?('cm')
-          value.to_i >= 150 && value.to_i <= 193
-        elsif value.end_with?('in')
-          value.to_i >= 59 && value.to_i <= 76
-        else
-          false
-        end
-      when 'hcl'
-        value.match(/^#[0-9a-f]{6}$/)
-      when 'ecl'
-        %w(amb blu brn gry grn hzl oth).include?(value)
-      when 'pid'
-        value.match(/^[0-9]{9}$/)
-      when 'cid'
-        true
-      else
-        false
-      end
-    end
+    (required_fields - passport.keys).empty? &&
+    (passport['byr'].length == 4 && passport['byr'].to_i >= 1920 && passport['byr'].to_i <= 2002) &&
+    (passport['iyr'].length == 4 && passport['iyr'].to_i >= 2010 && passport['iyr'].to_i <= 2020) &&
+    (passport['eyr'].length == 4 && passport['eyr'].to_i >= 2020 && passport['eyr'].to_i <= 2030) &&
+    (passport['hgt'].end_with?('cm') || passport['hgt'].end_with?('in')) &&
+    (!(passport['hgt'].end_with?('cm') ^ (passport['hgt'].to_i >= 150 && passport['hgt'].to_i <= 193))) &&
+    (!(passport['hgt'].end_with?('in') ^ (passport['hgt'].to_i >= 59 && passport['hgt'].to_i <= 76))) &&
+    (passport['hcl'].match(/^#[0-9a-f]{6}$/)) &&
+    (passport['ecl'].match(/^amb|blu|brn|gry|grn|hzl|oth$/)) &&
+    (passport['pid'].match(/^[0-9]{9}$/))
   end
 end
 
