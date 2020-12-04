dbf096b96ced49ebf015c68f542023b6a1d032dc
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-04 17:02:34 +0100

    Day 04

--- 2020/day-04/part2.rb
+++ 2020/day-04/part2.rb
@@ -5,12 +5,10 @@ def process(data)
     passport = d.split(/[\n\s]+/).map { |p| p.split(':') }.to_h
 
     (required_fields - passport.keys).empty? &&
-    (passport['byr'].length == 4 && passport['byr'].to_i >= 1920 && passport['byr'].to_i <= 2002) &&
-    (passport['iyr'].length == 4 && passport['iyr'].to_i >= 2010 && passport['iyr'].to_i <= 2020) &&
-    (passport['eyr'].length == 4 && passport['eyr'].to_i >= 2020 && passport['eyr'].to_i <= 2030) &&
-    (passport['hgt'].end_with?('cm') || passport['hgt'].end_with?('in')) &&
-    (!(passport['hgt'].end_with?('cm') ^ (passport['hgt'].to_i >= 150 && passport['hgt'].to_i <= 193))) &&
-    (!(passport['hgt'].end_with?('in') ^ (passport['hgt'].to_i >= 59 && passport['hgt'].to_i <= 76))) &&
+    (passport['byr'].match(/^(19[2-9][0-9])|(200[0-2])$/)) &&
+    (passport['iyr'].match(/^(201[0-9])|(2020)$/)) &&
+    (passport['eyr'].match(/^(202[0-9])|(2030)$/)) &&
+    (passport['hgt'].match(/^(1(([5-8][0-9])|(9[0-3]))cm)|(((59)|(6[0-9])|(7[0-6]))in)$/)) &&
     (passport['hcl'].match(/^#[0-9a-f]{6}$/)) &&
     (passport['ecl'].match(/^amb|blu|brn|gry|grn|hzl|oth$/)) &&
     (passport['pid'].match(/^[0-9]{9}$/))
