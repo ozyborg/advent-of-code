657ade220e3c83d4271d23aa3fb88ae1580e8891
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-06 07:46:16 +0100

    Day 06

---
+++ 2020/day-06/part2.rb
@@ -0,0 +1,11 @@
+def process(data)
+  data.inject(0) do |sum, d|
+    sum + d.split("\n").map(&:chars).reduce(:&).size
+  end
+end
+
+def input
+  File.read('./input.txt').split("\n\n")
+end
+
+puts process(input)
