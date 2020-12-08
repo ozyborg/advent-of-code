1c151ffb26abadf53ba7fce89d0bedf35f36163f
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-08 18:21:48 +0100

    Day 08

--- 2020/day-08/part2.rb
+++ 2020/day-08/part2.rb
@@ -3,7 +3,7 @@ def process(data)
   acc = 0
   history = []
   changed = []
-  can_be_changed = true
+  can_change = true
 
   loop do
     break if pointer == data.size
@@ -12,17 +12,17 @@ def process(data)
       pointer = 0
       acc = 0
       history = []
-      can_be_changed = true
+      can_change = true
     end
 
     opcode, arg = data[pointer].split(' ')
     arg = arg.to_i
 
-    if (opcode == 'nop' || opcode == 'jmp') && !changed.include?(pointer) && can_be_changed
+    if (opcode == 'nop' || opcode == 'jmp') && !changed.include?(pointer) && can_change
       opcode = 'jmp' if opcode == 'nop'
       opcode = 'nop' if opcode == 'jmp'
       changed << pointer
-      can_be_changed = false
+      can_change = false
     end
 
     history << pointer
