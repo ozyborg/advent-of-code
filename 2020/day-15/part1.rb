a2662e9000c08e636b2ac53417e213b225768e8f
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-15 18:49:06 +0100

    Day 15

---
+++ 2020/day-15/part1.rb
@@ -0,0 +1,22 @@
+def process(data)
+  positions = data.map.with_index { |d, i| [d, [nil, i]] }.to_h
+
+  spoken = data.last
+
+  (data.size..(2020 - 1)).each do |d|
+    if positions[spoken].first
+      spoken = positions[spoken].last - positions[spoken].first
+    else
+      spoken = 0
+    end
+    positions[spoken] = [positions[spoken]&.last, d]
+  end
+
+  spoken
+end
+
+def input
+  File.read('./input.txt').split(',').map(&:to_i)
+end
+
+puts process(input)
