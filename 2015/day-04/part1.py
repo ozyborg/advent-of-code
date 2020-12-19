d63a0b21f1885eb33665b60943358147b24beaf8
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-19 21:10:04 +0100

    Day 04

---
+++ 2015/day-04/part1.py
@@ -0,0 +1,14 @@
+import hashlib
+import itertools
+
+def input():
+  return open('./input.txt', 'r').read().strip()
+
+def process(data):
+  for i in itertools.count():
+    sequence = f'{data}{i}'.encode()
+    digest = hashlib.md5(sequence).hexdigest()
+    if digest.startswith('00000'):
+      return i
+
+print(process(input()))
