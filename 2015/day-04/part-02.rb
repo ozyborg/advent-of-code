bd2ea8377316eb90f17290724e287536c23a7248
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-26 17:16:20 +0100

    Day 04

---
+++ 2015/day-04/part-02.rb
@@ -0,0 +1,14 @@
+require 'digest'
+
+def process(data)
+  (1..).each do |i|
+    break i if Digest::MD5.hexdigest("#{data}#{i}").start_with?('000000')
+  end
+end
+
+def input
+  File.read(ARGV.first).chomp
+end
+
+puts process input
+
