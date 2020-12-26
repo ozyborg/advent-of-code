6a454a7475ef5a51dd5c1b5fd6d42e9a80372eac
Özlem Güzel <ozzlemguzzel@gmail.com>
2020-12-26 16:24:28 +0100

    Day 02

---
+++ 2015/day-02/part-01.rb
@@ -0,0 +1,17 @@
+def process(data)
+  data.inject(0) do |sum, lwh|
+    areas = lwh.combination(2)
+               .map { |d1, d2| d1 * d2 }
+
+    sum + 2 * areas.sum + areas.min
+  end
+end
+
+def input
+  File.read(ARGV.first)
+      .scan(/(\d+)x(\d+)x(\d+)/)
+      .map { |line| line.map(&:to_i) }
+end
+
+puts process input
+
---
+++ 2015/day-02/part-02.rb
@@ -0,0 +1,14 @@
+def process(data)
+  data.inject(0) do |sum, lwh|
+    sum + 2 * lwh.sort.first(2).sum + lwh.reduce(:*)
+  end
+end
+
+def input
+  File.read(ARGV.first)
+      .scan(/(\d+)x(\d+)x(\d+)/)
+      .map { |line| line.map(&:to_i) }
+end
+
+puts process input
+
