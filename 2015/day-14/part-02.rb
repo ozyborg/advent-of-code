d1fec58daaa6fb215661efcaebe0b4734edaa088
Özlem Güzel <ozzlemguzzel@gmail.com>
2021-12-02 19:10:07 +0100

    Day 14

---
+++ 2015/day-14/part-02.rb
@@ -0,0 +1,31 @@
+def process(data)
+  seconds = 2503
+
+  seqs = data.map do |d|
+    Enumerator.new do |y|
+      position = 0
+
+      loop do
+        d[2].times do
+          position += d[1]
+          y << position
+        end
+
+        d[3].times do
+          y << position
+        end
+      end
+    end
+  end
+
+  seqs.map { |s| s.take(seconds) }.transpose.map { |s| s.each_with_index.max.last }.tally.values.max
+end
+
+def input
+  File.read(ARGV.first)
+      .scan(/(\w+) can fly (\w+) km\/s for (\w+) seconds, but then must rest for (\w+) seconds.\n/)
+      .map { |d| [d[0], d[1].to_i, d[2].to_i, d[3].to_i] }
+end
+
+puts process(input)
+
