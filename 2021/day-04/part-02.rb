4af69ba389c5acc27a1e8c410c6e1875f1c1fba9
Özlem Güzel <ozzlemguzzel@gmail.com>
2021-12-04 22:26:23 +0100

    Day 04

--- 2021/day-04/part-02.rb
+++ 2021/day-04/part-02.rb
@@ -1,14 +1,14 @@
 def process(data)
-  row_board_map = data[:boards].flatten(1).map.with_index { |v, i| [v, i / 5] }.to_h
-  col_board_map = data[:boards].map(&:transpose).flatten(1).map.with_index { |v, i| [v, i / 5] }.to_h
+  row_board_map = data[:boards].flatten(1).map.with_index { |v, i| [v, i / 5] }
+  col_board_map = data[:boards].map(&:transpose).flatten(1).map.with_index { |v, i| [v, i / 5] }
 
   winners = []
 
   data[:numbers].size.times do |i|
     subset = data[:numbers].first(i + 1)
 
-    winners |= row_board_map.filter { |k, v| !winners.include?(v) && (k - subset).empty? }.map(&:last)
-    winners |= col_board_map.filter { |k, v| !winners.include?(v) && (k - subset).empty? }.map(&:last)
+    winners |= row_board_map.filter { |k, v| (k - subset).empty? }.map(&:last)
+    winners |= col_board_map.filter { |k, v| (k - subset).empty? }.map(&:last)
 
     break (data[:boards][winners.last].flatten - subset).sum * subset.last if winners.size == data[:boards].size
   end
@@ -19,9 +19,7 @@ def input
 
   {
     numbers: numbers.split(',').map(&:to_i),
-    boards: boards.map do |b|
-      b.split("\n").map { |l| l.split(' ').map(&:to_i) }
-    end
+    boards: boards.map { |b| b.split("\n").map { |l| l.split(' ').map(&:to_i) } }
   }
 end
 
