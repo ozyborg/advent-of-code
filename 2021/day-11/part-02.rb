d3cea70cc0f8422dfa6d8fb45602dea21d815bb7
Özlem Güzel <ozzlemguzzel@gmail.com>
2021-12-11 09:37:45 +0100

    Day 11

---
+++ 2021/day-11/part-02.rb
@@ -0,0 +1,47 @@
+def process(data)
+  (0..).each do |i|
+    break i if data.values.all?(0)
+
+    next_data = data.transform_values(&:next)
+
+    flashed = {}
+
+    loop do
+      flashes = next_data.filter { |k, v| v > 9 }
+
+      break if flashes.empty?
+
+      flashed.merge!(flashes)
+
+      flashes.each do |k, v|
+        next_data[k] = 0
+
+        x, y = k
+
+        adjs = [
+          [x - 1, y - 1], [x - 1, y], [x - 1, y + 1],
+          [x, y - 1], [x, y + 1],
+          [x + 1, y - 1], [x + 1, y], [x + 1, y + 1]
+        ]
+
+        adjs.each do |a|
+          next_data[a] = next_data[a].next if next_data.key?(a) && !flashed.key?(a)
+        end
+      end
+    end
+
+    data = next_data
+  end
+end
+
+def input
+  File.readlines(ARGV.first)
+      .map { |l| l.chomp.chars.map(&:to_i) }
+      .map.with_index do |row, x|
+        row.map.with_index do |col, y|
+          [[x, y], col]
+        end
+      end.flatten(1).to_h
+end
+
+puts process(input)
