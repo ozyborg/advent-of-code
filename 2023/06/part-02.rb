350590d500e2d9d0f7923ec01ad455632d3d9cd1
Özlem Güzel <ozzlemguzzel@gmail.com>
2023-12-06 10:07:13 +0100

    Day 06

---
+++ 2023/06/part-02.rb
@@ -0,0 +1,25 @@
+class Puzzle
+  def input
+    @input ||= File.read('./input.txt')
+  end
+
+  def data
+    @data ||= input.split("\n").map do |line|
+      key, *values = line.split
+
+      [values.join.to_i]
+    end
+  end
+
+  def solve
+    times, distances = data
+
+    (times.zip(distances)).map do |time, distance|
+      (time / 2 + 1..).take_while do |t|
+        t * (time - t) > distance
+      end.count * 2 + (time.even? ? 1 : 0)
+    end.reduce(:*)
+  end
+end
+
+puts Puzzle.new.solve
